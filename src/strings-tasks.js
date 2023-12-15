38c909e0aab18ac625431b1ff7ee8711806e078e
Alexandra Paramonova <alpar.wb@gmail.com>
2023-12-15 21:48:49 +0500

    fix: implement extractNameFromTemplate

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -469,8 +469,12 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const templateStart = 'Hello, ';
+  const templateEnd = '!';
+  const valueRemoveStart = value.replace(templateStart, '');
+  const valueFirstAndLastNames = valueRemoveStart.replace(templateEnd, '');
+  return valueFirstAndLastNames;
 }
 
 /**
